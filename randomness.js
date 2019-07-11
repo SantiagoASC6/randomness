3dccbc8914a3d80c0c7b964011896f3a6dd35f64
SantiagoASC6 <sanosdu15@gmail.com>
2019-07-11 11:57:10 -0400

    first draft

---
+++ randomness.js
@@ -0,0 +1,33 @@
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+const foods = [
+    "pizza",
+    "burgers",
+    "rice and beans",
+    "lasagna",
+    "macatoni and cheese",
+    "baked potatoes",
+];
+const studetns = [
+    "Bob",
+    "Martin",
+    "Danny",
+    "Mahdi",
+    "Harry",
+    "Sean",
+    "Robert"
+];
+function randNumber() {
+    const decimal = Math.random();
+    //console.log(decimal);
+    const times21 = decimal * 21;
+    //console.log(times21);
+    const final = Math.floor(times21);
+    console.log(final);
+}
+randNumber() 
+
+function randLetter() {
+    const alphabet = 
+    console.log(alphabet)
+}
+randLetter()
